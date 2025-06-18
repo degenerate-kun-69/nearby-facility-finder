384a7365867567638a919e8feb352956709defe1
Srrijan Arora <sabhaya5567@gmail.com>
2025-06-18 15:42:05 +0530

    Fixed wrong API call

--- api_handler.py
+++ api_handler.py
@@ -9,15 +9,25 @@ class GeoapifyHandler:
 
     def geocode(self, address):
         url = f"https://api.geoapify.com/v1/geocode/search?text={urllib.parse.quote(address)}&apiKey={self.api_key}"
+        print("[DEBUG] Request URL:", url)
         with urllib.request.urlopen(url) as response:
             data = json.loads(response.read())
             coords = data["features"][0]["geometry"]["coordinates"]
             return coords[1], coords[0]  # lat, lon
 
     def get_nearby_facilities(self, lat, lon, types):
+        category_map={
+            "hospital": "healthcare.hospital",
+            "school": "education.school",
+            "police": "service.police"
+        }
         all_facilities = []
         for type_ in types:
-            url = f"https://api.geoapify.com/v2/places?categories=amenity.{type_}&filter=circle:{lon},{lat},2000&limit=5&apiKey={self.api_key}"
+            category=category_map.get(type_)
+            if not category:
+                print(f"[ERROR] Unsupported type: {type_}")
+                continue
+            url = f"https://api.geoapify.com/v2/places?categories={category}&filter=circle:{lon},{lat},2000&limit=5&apiKey={self.api_key}" #for hospitals, need healthcare.hospital
             with urllib.request.urlopen(url) as response:
                 data = json.loads(response.read())
                 for f in data["features"]:
--- config.py
+++ config.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_KEY = os.getenv("GEOAPIFY_API_KEY")
+print("Loaded API KEY:", API_KEY)
 
 if not API_KEY:
     raise ValueError("Missing GEOAPIFY_API_KEY in .env file")
