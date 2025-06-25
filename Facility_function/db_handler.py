2d2e1ba93f5786cbcdec833918f9f6e69f23c4fc
Srrijan Arora <sabhaya5567@gmail.com>
2025-06-25 21:14:53 +0530

    updated db_handler.py to be compatible with cosmos db

--- Facility_function/db_handler.py
+++ Facility_function/db_handler.py
@@ -1,59 +1,92 @@
-# db_handler.py
-import sqlite3
+# db_handler.py updated for Azure Cosmos DB integration
+import os
 import json
+import uuid
+from azure.cosmos import CosmosClient, PartitionKey
 
 class DBHandler:
-    def __init__(self, db_name="facilities.db"):
-        self.conn = sqlite3.connect(db_name)
-        self.create_tables()
+    def __init__(self):
+        # Read from environment variables
+        self.endpoint = os.getenv("COSMOS_URI")
+        self.key = os.getenv("COSMOS_KEY")
 
-    def create_tables(self):
-        self.conn.execute('''
-            CREATE TABLE IF NOT EXISTS facilities (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                address TEXT,
-                name TEXT,
-                type TEXT,
-                lat REAL,
-                lon REAL
-            )
-        ''')
-        self.conn.execute('''
-            CREATE TABLE IF NOT EXISTS location_cache (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                address TEXT,
-                type TEXT,
-                fetched BOOLEAN,
-                lat REAL,
-                lon REAL,
-                raw_json TEXT
-            )
-        ''')
-        self.conn.commit()
+        if not self.endpoint or not self.key:
+            raise ValueError("Missing COSMOS_URI or COSMOS_KEY environment variables")
+
+        self.client = CosmosClient(self.endpoint, self.key)
+        self.database_name = "NearbyFacilitiesDB"
+        self.facility_container_name = "Facilities"
+        self.cache_container_name = "LocationCache"
+
+        self._setup_db()
+
+    def _setup_db(self):
+        # Create DB and containers if they don't exist
+        self.database = self.client.create_database_if_not_exists(self.database_name)
+
+        self.facility_container = self.database.create_container_if_not_exists(
+            id=self.facility_container_name,
+            partition_key=PartitionKey(path="/address"),
+            offer_throughput=400
+        )
+
+        self.cache_container = self.database.create_container_if_not_exists(
+            id=self.cache_container_name,
+            partition_key=PartitionKey(path="/address"),
+            offer_throughput=400
+        )
 
     def insert_facility(self, address, name, type_, lat, lon):
-        self.conn.execute('''
-            INSERT INTO facilities (address, name, type, lat, lon)
-            VALUES (?, ?, ?, ?, ?)
-        ''', (address, name, type_, lat, lon))
-        self.conn.commit()
+        item = {
+            "id": str(uuid.uuid4()),
+            "address": address,
+            "name": name,
+            "type": type_,
+            "lat": lat,
+            "lon": lon
+        }
+        self.facility_container.upsert_item(item)
 
     def get_cached_facilities(self, address, type_):
-        cur = self.conn.execute('''
-            SELECT name, type, lat, lon FROM facilities
-            WHERE address = ? AND type = ?
-        ''', (address, type_))
-        return cur.fetchall()
+        query = f"""
+        SELECT c.name, c.type, c.lat, c.lon FROM c
+        WHERE c.address = @address AND c.type = @type
+        """
+        parameters = [
+            {"name": "@address", "value": address},
+            {"name": "@type", "value": type_}
+        ]
+        results = list(self.facility_container.query_items(
+            query=query,
+            parameters=parameters,
+            enable_cross_partition_query=True
+        ))
+        return [(r['name'], r['type'], r['lat'], r['lon']) for r in results]
 
     def is_cached(self, address, type_):
-        cur = self.conn.execute('''
-            SELECT fetched FROM location_cache WHERE address = ? AND type = ?
-        ''', (address, type_))
-        return cur.fetchone() is not None
+        query = f"""
+        SELECT VALUE c.fetched FROM c
+        WHERE c.address = @address AND c.type = @type
+        """
+        parameters = [
+            {"name": "@address", "value": address},
+            {"name": "@type", "value": type_}
+        ]
+        results = list(self.cache_container.query_items(
+            query=query,
+            parameters=parameters,
+            enable_cross_partition_query=True
+        ))
+        return len(results) > 0
 
     def cache_location(self, address, type_, lat, lon, raw_json):
-        self.conn.execute('''
-            INSERT INTO location_cache (address, type, fetched, lat, lon, raw_json)
-            VALUES (?, ?, 1, ?, ?, ?)
-        ''', (address, type_, lat, lon, json.dumps(raw_json)))
-        self.conn.commit()
+        item = {
+            "id": str(uuid.uuid4()),
+            "address": address,
+            "type": type_,
+            "fetched": True,
+            "lat": lat,
+            "lon": lon,
+            "raw_json": json.dumps(raw_json)
+        }
+        self.cache_container.upsert_item(item)
